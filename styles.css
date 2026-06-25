4f40abbb90789969e68fe06b2f9952ad087551d9
karenpearlx <phthebookvault@gmail.com>
2026-06-25 02:49:34 -0700

    Initial commit - Book Vault PH site

---
+++ styles.css
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <style type="text/css">
+            :root {
+                color-scheme: light dark;
+            }
+        </style>
+        <title>Error response</title>
+    </head>
+    <body>
+        <h1>Error response</h1>
+        <p>Error code: 404</p>
+        <p>Message: File not found.</p>
+        <p>Error code explanation: 404 - Nothing matches the given URI.</p>
+    </body>
+</html>
